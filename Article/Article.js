e04c4e542045186a1c2faa7da9a82e82827855b3
droneal11015 <droneal11015@gmail.com>
2019-04-17 16:08:35 -0400

    imported article.less file for article styling, modified article.js file to clean up code

--- Article/Article.js
+++ Article/Article.js
@@ -7,13 +7,11 @@ class Article {
     // create a reference to the ".expandButton" class. 
     this.expandButton = this.domElement.querySelector(".expandButton");
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    this.expandButton.textContent = "Click to expand";
-
+    this.expandButton.textContent = "expand";
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    this.expandButton.addEventListener("click", event => this.expandArticle(event));
-
+    this.expandButton.addEventListener('click', () => this.expandArticle());
+    
   }
-
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
     this.domElement.classList.toggle("article-open");
@@ -28,6 +26,6 @@ class Article {
 
 */
 let articles = document.querySelectorAll(".article");
-articles.forEach(article => {
-  return new Article(article);  
+articles.forEach(function(article){
+  return new Article(article)
 });
