af4903c77a204b6fbee8e902c5a296c8eb7b9de8
droneal11015 <droneal11015@gmail.com>
2019-04-17 15:54:41 -0400

    added toggle to expand or hide article, added for each for new articles

--- Article/Article.js
+++ Article/Article.js
@@ -7,14 +7,16 @@ class Article {
     // create a reference to the ".expandButton" class. 
     this.expandButton = this.domElement.querySelector(".expandButton");
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    
+    this.expandButton.textContent = "Click to expand";
+
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    
+    this.expandButton.addEventListener("click", event => this.expandArticle(e));
+
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-
+    this.domElement.classList.toggle("article-is-open");
   }
 }
 
@@ -25,5 +27,7 @@ class Article {
 - With your selection in place, now chain .forEach() on to the articles variable to iterate over the articles NodeList and create a new instance of Article by passing in each article as a parameter to the Article class.
 
 */
-
-let articles;
+let articles = document.querySelectorAll(".article");
+articles.forEach(article => {
+  return new Article(article);  
+});
