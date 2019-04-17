c51835e39f0fd67a4a7b0da32332adaba9de921d
droneal11015 <droneal11015@gmail.com>
2019-04-17 15:57:31 -0400

    fixed some grammatical errors

--- Article/Article.js
+++ Article/Article.js
@@ -10,13 +10,13 @@ class Article {
     this.expandButton.textContent = "Click to expand";
 
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    this.expandButton.addEventListener("click", event => this.expandArticle(e));
+    this.expandButton.addEventListener("click", event => this.expandArticle(event));
 
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-    this.domElement.classList.toggle("article-is-open");
+    this.domElement.classList.toggle("article-open");
   }
 }
 
