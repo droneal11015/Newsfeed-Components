590b54a78003ef8e1344efe68e275795adb70050
droneal11015 <droneal11015@gmail.com>
2019-04-17 19:21:22 -0400

    attempted animation

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -3,6 +3,7 @@ const toggleMenu = () => {
   // Toggle the "menu--open" class on your menu refence. 
   menu.classList.toggle('menu--open');
 
+
 }
 
 // Start Here: Create a reference to the ".menu" class
@@ -14,3 +15,9 @@ const menuButton = document.querySelector('.menu-button');
 
 // Using your menuButton reference, add a click handler that calls toggleMenu
 menuButton.addEventListener('click', toggleMenu);
+
+
+
+//animation
+const menuOpening = document.querySelector(".menu--open");
+
